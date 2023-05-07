dd3486f22672f4ddcf4a8d4e74ca5949d412cccb
ReaperSigma <vidium400@gmail.com>
2023-05-07 16:26:36 +0200

    drivers:media:platform:msm fix conflicts after merging latest codelinaro tag

--- drivers/media/platform/msm/synx/synx_util.c
+++ drivers/media/platform/msm/synx/synx_util.c
@@ -245,7 +245,6 @@ int synx_deinit_object(struct synx_table_row *row)
 		}
 	}
 
-
 	memset(row, 0, sizeof(*row));
 	clear_bit(index, synx_dev->bitmap);
 
@@ -574,7 +573,7 @@ void *synx_from_handle(s32 synx_obj)
 		return NULL;
 	}
 
-	base = (current->tgid << 16) & 0x7FFFFFFF;
+	base = current->tgid << 16;
 
 	if ((base >> 16) != (synx_obj >> 16)) {
 		pr_err("current client: %d, base: %d, synx_obj: 0x%x\n",
@@ -587,7 +586,7 @@ void *synx_from_handle(s32 synx_obj)
 
 s32 synx_create_handle(void *pObj)
 {
-	s32 base = (current->tgid << 16) & 0x7FFFFFFF;
+	s32 base = current->tgid << 16;
 	s32 id;
 	unsigned long flags;
 
