4a0336685238c89fc447809f8ea468848e88c0e2
ReaperSigma <vidium400@gmail.com>
2023-06-23 23:15:32 +0200

    INCLUDE: add missing macros

--- include/linux/sched/cpufreq.h
+++ include/linux/sched/cpufreq.h
@@ -16,6 +16,8 @@
 #define SCHED_CPUFREQ_EARLY_DET (1U << 6)
 #define SCHED_CPUFREQ_CONTINUE (1U << 8)
 
+#define SCHED_CPUFREQ_BOOST_UPDATE	(1U << 9)
+
 #ifdef CONFIG_CPU_FREQ
 struct cpufreq_policy;
 
@@ -27,13 +29,19 @@ void cpufreq_add_update_util_hook(int cpu, struct update_util_data *data,
                        void (*func)(struct update_util_data *data, u64 time,
 				    unsigned int flags));
 void cpufreq_remove_update_util_hook(int cpu);
-bool cpufreq_this_cpu_can_update(struct cpufreq_policy *policy);
 
 static inline unsigned long map_util_freq(unsigned long util,
 					unsigned long freq, unsigned long cap)
 {
 	return (freq + (freq >> 2)) * util / cap;
 }
+bool cpufreq_this_cpu_can_update(struct cpufreq_policy *policy);
+
+/*static inline unsigned long map_util_freq(unsigned long util,
+					unsigned long freq, unsigned long cap)
+{
+	return (freq + (freq >> 2)) * util / cap;
+}*/
 #endif /* CONFIG_CPU_FREQ */
 
 #endif /* _LINUX_SCHED_CPUFREQ_H */
